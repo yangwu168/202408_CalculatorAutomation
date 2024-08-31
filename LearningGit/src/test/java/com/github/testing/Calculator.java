aba7e778232e64e86e6a20a06dd7dc3e7eaac353
yangwu168 <wenyangwu168@gmail.com>
2024-08-30 22:21:03 -0500

    Adding hello world print statement, so it can display in the console

--- LearningGit/src/test/java/com/github/testing/Calculator.java
+++ LearningGit/src/test/java/com/github/testing/Calculator.java
@@ -3,7 +3,8 @@ package com.github.testing;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
 
 	}
 
